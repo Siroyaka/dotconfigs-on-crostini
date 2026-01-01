0d75def7826d3d97e38de134093a90d86e980459
siroyaka <29934865+Siroyaka@users.noreply.github.com>
2026-01-01 16:45:16 +0900

    add nvim make symboliclink function

--- make-symboliclink.sh
+++ make-symboliclink.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
+function make_file_link () {
+	src=$1
+	dst=$2
+
+	if [[ ! -f $src ]]; then
+		exit
+	fi
+
+	if [[ ! -e $(dirname $dst) ]]; then
+		mkdir -p $(dirname $dst)
+	fi
+
+	ln -sf $src $dst
+}
+
+function make_dir_link () {
+	src=$1
+	dst=$2
+
+	if [[ ! -d $src ]]; then
+		exit
+	fi
+
+	if [[ ! -e $(dirname $dst) ]]; then
+		mkdir -p $(dirname $dst)
+	fi
+
+	ln -nsf $src $dst
+}
+
+make_file_link ./config/nvim/init.lua ~/.config/nvim/init.lua
+make_dir_link ./config/nvim/lua ~/.config/nvim/lua
 
